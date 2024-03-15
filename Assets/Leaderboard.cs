08617be95c821ad17610523b1c3329b0c2dc8221
SI\Joshua.Henderson <joshua.henderson@sigames.com>
2024-03-15 22:18:44 +0000

    Snappier leaderboard camera

--- Assets/Leaderboard.cs
+++ Assets/Leaderboard.cs
@@ -2,19 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Leaderboard : MonoBehaviour
 {
-
     private void OnEnable()
     {
         Brain.ins.EventHandler._endRoundEvent.AddListener(SyncLeaderboard);
-
     }
 
     private void SyncLeaderboard(RoundEndCondition context)
     {
-        Debug.LogFormat("Leaderboard received a sync call from an round with the {0} win condition");
+        Debug.LogFormat("Leaderboard received a sync call from an round with the {0} win condition", context.ToString());
     }
-
-
 }
--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -4,6 +4,7 @@ public class MainMenu : MonoBehaviour
 {
     public void RequestNewRound()
     {
+
         Brain.ins.SceneHandler.LoadScenes(new List<string>() { "Round", "Leaderboard", "GameScene" });
         Brain.ins.SceneHandler.UnloadScenes(new List<string>() { "MainMenu" });
     }
