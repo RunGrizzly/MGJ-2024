359380f14c3c776e87e3bb7342c33a585f6cdc12
Atuuh <not-another-one@hotmail.com>
2024-03-17 06:15:29 +0000

    Added ante loss mechanic

--- Assets/LeaderboardLabel.cs
+++ Assets/LeaderboardLabel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -9,6 +10,20 @@ public class LeaderboardLabel : MonoBehaviour
     //   [SerializeField] private Camera _lbCamera = null;
     [field: SerializeField] public TextMeshProUGUI NameBox { get; set; } = null;
 
+    private Round _round;
+
+    private void OnEnable()
+    {
+        Brain.ins.EventHandler.RoundLostEvent.AddListener(OnRoundLost);
+    }
+
+    private void OnRoundLost(Round round)
+    {
+        if (round == _round)
+        {
+            Destroy(gameObject);
+        }
+    }
     // void FixedUpdate()
     // {
     //     transform.rotation = Quaternion.LookRotation(_lbCamera.transform.position - transform.position);
